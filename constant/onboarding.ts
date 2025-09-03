f09142ed003d445f4b96d22b66686f81757033bf
eaaaarl <dev.earl00@gmail.com>
2025-09-03 19:31:01 +0800

    feat: implement a onboarding flow for new user

---
+++ constant/onboarding.ts
@@ -0,0 +1,37 @@
+import onboarding1 from "@/assets/images/onboarding1.png";
+import onboarding2 from "@/assets/images/onboarding2.png";
+import onboarding3 from "@/assets/images/onboarding3.png";
+
+const images = {
+  onboarding1,
+  onboarding2,
+  onboarding3,
+};
+
+export const onboarding = [
+  {
+    id: 1,
+    title: "Best Bao-Bao in your hands with BaoGO",
+    description:
+      "Discover the convenience of finding your perfect ride with BaoGO",
+    image: images.onboarding1,
+  },
+  {
+    id: 2,
+    title: "The perfect ride is just a tap away!",
+    description:
+      "Your journey begins with BaoGO. Find your ideal ride effortlessly.",
+    image: images.onboarding2,
+  },
+  {
+    id: 3,
+    title: "Your ride, your way. Let's go!",
+    description:
+      "Enter your destination, sit back, and let us take care of the rest.",
+    image: images.onboarding3,
+  },
+];
+
+export const data = {
+  onboarding,
+};
